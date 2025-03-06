8bffc66e3bdd425451ec1badfe9f7418d44e89fb
Matt <117805836+Staber07@users.noreply.github.com>
2025-03-07 01:32:21 +0800

    UI - progress Bar, card done.

--- app/src/main/java/com/example/pisowisefinal/models/Expense.kt
+++ app/src/main/java/com/example/pisowisefinal/models/Expense.kt
@@ -1,11 +1,13 @@
 package com.example.pisowisefinal.models
 
+import com.example.pisowisefinal.utils.Constants
+
 data class Expense(
-    val id: Int? = null,  // Nullable since it’s auto-incremented
+    val id: Int,  // Make it non-nullable to ensure deletion works properly
     val category: String,
     val date: String,
     val amount: Double,
-    val transactionType: String? = null,  // Nullable, avoids storing empty strings
-    val title: String? = null,
-    val message: String? = null
+    val transactionType: String = Constants.DEFAULT_TRANSACTION_TYPE,
+    val title: String = Constants.DEFAULT_TITLE,
+    val message: String = Constants.DEFAULT_MESSAGE
 )
